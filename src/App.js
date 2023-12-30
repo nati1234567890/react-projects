640380f649392f8adc7dad141be7318b489fd2e7
natnael <natiizelalem@gmail.com>
2023-12-29 23:58:50 -0800

    finished the load-more project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordion from "./components/accoridian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
+import LoadMore from "./components/load-more-data";
 
 function App() {
   return (
@@ -11,11 +12,12 @@ function App() {
       {/* <Accordion /> */}
       {/* <RandomColor /> */}
       {/* <StarRating /> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      <LoadMore />
     </div>
   );
 }
